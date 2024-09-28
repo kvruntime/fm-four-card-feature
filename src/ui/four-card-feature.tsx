9e4c541616d28f79e7c661e3ad6938df9cfb1d47
kvruntime <kvruntime@gmail.com>
2024-09-28 20:40:24 +0000

    fix border color on card

--- src/ui/four-card-feature.tsx
+++ src/ui/four-card-feature.tsx
@@ -1,9 +1,8 @@
-import React from "react"
+import "./four-card-feature.css"
 import calclator from "/public/images/icon-calculator.svg"
 import karma from "/public/images/icon-karma.svg"
 import supervisor from "/public/images/icon-supervisor.svg"
 import teamBuilder from "/public/images/icon-team-builder.svg"
-import "./four-card-feature.css"
 
 
 interface Item {
@@ -16,51 +15,68 @@ interface Item {
 
 function FourCardFeature() {
   const items: Item[] = [
-    { title: "Supervisor", description: "Monitors activity to identify project roadblocks", imageUrl: supervisor, borderColor: "--cyan" },
-    { title: "Team Builder", description: "Scans our talent network to create the optimal team for your project", imageUrl: teamBuilder, borderColor: "--c-red" },
-    { title: "Karma", description: "Regularly evaluates our talent to ensure quality", imageUrl: karma, borderColor: "--orange" },
-    { title: "Calculator", description: "Uses data from past projects to provide better delivery estimates", imageUrl: calclator, borderColor: "--blue" },
+    {
+      title: "Supervisor", description: "Monitors activity to identify project roadblocks", imageUrl: supervisor,
+      borderColor: "--color-cyan"
+    },
+    {
+      title: "Team Builder", description: "Scans our talent network to create the optimal team for your project", imageUrl: teamBuilder,
+      borderColor: "--color-red"
+    },
+    {
+      title: "Karma", description: "Regularly evaluates our talent to ensure quality", imageUrl: karma,
+      borderColor: "--color-orange"
+    },
+    {
+      title: "Calculator", description: "Uses data from past projects to provide better delivery estimates", imageUrl: calclator,
+      borderColor: "--color-blue"
+    },
   ]
-  return (<section className="flex flex-col items-center">
+  return (
+    <section className="flex flex-col items-center px-7 my-10 py-10">
 
-    <div className="text-center">
-      <p className="text-[--grayish-blue]">Reliable, efficient delivery</p>
-      <h2 className="font-[600]">Powered by Technology</h2>
+      <div className="text-center mb-6">
+        <p className="text-[--grayish-blue] text-xl">Reliable, efficient delivery</p>
+        <h2 className="font-[600] text-[--very-dark-blue] text-2xl my-2">Powered by Technology</h2>
 
-      <p className="text-[--grayish-blue]">
-        Our Artificial Intelligence powered tools use millions of project data points
-        to ensure that your project is successful
-      </p>
-    </div>
-
-
-    <div className="grid grid-cols-1 gap-20 w-[300px] rounded-lg overflow-hidden">
-      {items.map((item, index) =>
-        <article key={index}
-          className={[
-            "technology-card shadow-lg p-6 flex flex-col border-t-[4px]",
-            `border-[${item.borderColor ?? "--red"}]`
-          ].join(" ")}
-        >
-          <h3 className="font-[600]">{item.title}</h3>
-          <p className="text-[--grayish-blue]">{item.description}</p>
-          <picture className="flex flex-row justify-end">
-            <img src={item.imageUrl} alt={item.title} />
-          </picture>
-        </article>)}
-    </div>
+        <p className="header-description text-[--grayish-blue]">
+          Our Artificial Intelligence powered tools use millions of project data points
+          to ensure that your project is successful
+        </p>
+      </div>
 
 
+      <div className="card-container grid grid-cols-1 md:grid-cols-3 md:grid-rows-4 gap-10  gap rounded-lg overflow-hidden my-15">
+        {items.map((item, index) =>
 
+          <article key={index}
+            className={"".concat(
+              ...[
+                "technology-card shadow-lg p-6 flex flex-col  rounded-lg overflow-hidden border-t-[4px] ",
+              ]
+            )}
+            style={{ borderTop: `4px solid ${item.borderColor ? `var(${item.borderColor})` : `var(--very-dark-blue)`}` }}
+          >
 
-
-
-
+            <h3 className="font-[600]">{item.title}</h3>
+            <p className=" text-[--grayish-blue]">{item.description}</p>
+            <picture className="flex flex-row justify-end mt-4">
+              <img src={item.imageUrl} alt={item.title} />
+            </picture>
+          </article>)}
+      </div>
 
 
 
 
-  </section>);
+
+
+
+
+
+
+
+    </section >);
 }
 
 export default FourCardFeature;
